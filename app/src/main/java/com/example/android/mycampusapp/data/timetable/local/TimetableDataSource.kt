43659fc5e2f5d38bcca3110ae74a42e394470db6
Raphael Ndonga <nkibi53@gmail.com>
2020-06-24 18:05:15 +0300

    The first upload to github. Still unsure whether the database actually works. Will implement fragments to check that out manually

--- app/src/main/java/com/example/android/mycampusapp/data/timetable/local/TimetableDataSource.kt
+++ app/src/main/java/com/example/android/mycampusapp/data/timetable/local/TimetableDataSource.kt
@@ -5,47 +5,52 @@ import com.example.android.mycampusapp.data.*
 
 interface TimetableDataSource {
 
-    fun addMondayClass(mondayClass: MondayClass)
-    fun addTuesdayClass(tuesdayClass: TuesdayClass)
-    fun addWednesdayClass(wednesdayClass: WednesdayClass)
-    fun addThursdayClass(thursdayClass: ThursdayClass)
-    fun addFridayClass(fridayClass: FridayClass)
-    fun addSaturdayClass(saturdayClass: SaturdayClass)
-    fun addSundayClass(sundayClass: SundayClass)
+    suspend fun addMondayClass(mondayClass: MondayClass)
+    suspend fun addTuesdayClass(tuesdayClass: TuesdayClass)
+    suspend fun addWednesdayClass(wednesdayClass: WednesdayClass)
+    suspend fun addThursdayClass(thursdayClass: ThursdayClass)
+    suspend fun addFridayClass(fridayClass: FridayClass)
+    suspend fun addSaturdayClass(saturdayClass: SaturdayClass)
+    suspend fun addSundayClass(sundayClass: SundayClass)
 
-    fun deleteMondayClass(mondayClass: MondayClass)
-    fun deleteTuesdayClass(tuesdayClass: TuesdayClass)
-    fun deleteWednesdayClass(wednesdayClass: WednesdayClass)
-    fun deleteThursdayClass(thursdayClass: ThursdayClass)
-    fun deleteFridayClass(fridayClass: FridayClass)
-    fun deleteSaturdayClass(saturdayClass: SaturdayClass)
-    fun deleteSundayClass(sundayClass: SundayClass)
+    suspend fun deleteMondayClass(mondayClass: MondayClass)
+    suspend fun deleteTuesdayClass(tuesdayClass: TuesdayClass)
+    suspend fun deleteWednesdayClass(wednesdayClass: WednesdayClass)
+    suspend fun deleteThursdayClass(thursdayClass: ThursdayClass)
+    suspend fun deleteFridayClass(fridayClass: FridayClass)
+    suspend fun deleteSaturdayClass(saturdayClass: SaturdayClass)
+    suspend fun deleteSundayClass(sundayClass: SundayClass)
 
-    fun updateMondayClass(mondayClass: MondayClass)
-    fun updateTuesdayClass(tuesdayClass: TuesdayClass)
-    fun updateWednesdayClass(wednesdayClass: WednesdayClass)
-    fun updateThursdayClass(thursdayClass: ThursdayClass)
-    fun updateFridayClass(fridayClass: FridayClass)
-    fun updateSaturdayClass(saturdayClass: SaturdayClass)
-    fun updateSundayClass(sundayClass: SundayClass)
+    suspend fun updateMondayClass(mondayClass: MondayClass)
+    suspend fun updateTuesdayClass(tuesdayClass: TuesdayClass)
+    suspend fun updateWednesdayClass(wednesdayClass: WednesdayClass)
+    suspend fun updateThursdayClass(thursdayClass: ThursdayClass)
+    suspend fun updateFridayClass(fridayClass: FridayClass)
+    suspend fun updateSaturdayClass(saturdayClass: SaturdayClass)
+    suspend fun updateSundayClass(sundayClass: SundayClass)
 
-    fun observeAllMondayClasses():LiveData<List<MondayClass>>
-    fun observeAllTuesdayClasses():LiveData<List<TuesdayClass>>
-    fun observeAllWednesdayClasses():LiveData<List<WednesdayClass>>
-    fun observeAllThursdayClasses():LiveData<List<ThursdayClass>>
-    fun observeAllFridayClasses():LiveData<List<FridayClass>>
-    fun observeAllSaturdayClasses():LiveData<List<SaturdayClass>>
-    fun observeAllSundayClasses():LiveData<List<SundayClass>>
+    suspend fun observeAllMondayClasses():LiveData<List<MondayClass>>
+    suspend fun observeAllTuesdayClasses():LiveData<List<TuesdayClass>>
+    suspend fun observeAllWednesdayClasses():LiveData<List<WednesdayClass>>
+    suspend fun observeAllThursdayClasses():LiveData<List<ThursdayClass>>
+    suspend fun observeAllFridayClasses():LiveData<List<FridayClass>>
+    suspend fun observeAllSaturdayClasses():LiveData<List<SaturdayClass>>
+    suspend fun observeAllSundayClasses():LiveData<List<SundayClass>>
 
-    fun deleteAllMondayClasses(mondayClass: MondayClass)
-    fun deleteAllTuesdayClasses(tuesdayClass: TuesdayClass)
-    fun deleteAllWednesdayClasses(wednesdayClass: WednesdayClass)
-    fun deleteAllThursdayClasses(thursdayClass: ThursdayClass)
-    fun deleteAllFridayClasses(fridayClass: FridayClass)
-    fun deleteAllSaturdayClasses(saturdayClass: SaturdayClass)
-    fun deleteAllSundayClasses(sundayClass: SundayClass)
+    suspend fun deleteAllMondayClasses(mondayClass: MondayClass)
+    suspend fun deleteAllTuesdayClasses(tuesdayClass: TuesdayClass)
+    suspend fun deleteAllWednesdayClasses(wednesdayClass: WednesdayClass)
+    suspend fun deleteAllThursdayClasses(thursdayClass: ThursdayClass)
+    suspend fun deleteAllFridayClasses(fridayClass: FridayClass)
+    suspend fun deleteAllSaturdayClasses(saturdayClass: SaturdayClass)
+    suspend fun deleteAllSundayClasses(sundayClass: SundayClass)
 
-    fun getMondayClassWithId(id:Long):MondayClass?
-
+    suspend fun getAllMondayClasses():List<MondayClass>
+    suspend fun getAllTuesdayClasses():List<MondayClass>
+    suspend fun getAllWednesdayClasses():List<MondayClass>
+    suspend fun getAllThursdayClasses():List<MondayClass>
+    suspend fun getAllFridayClasses():List<MondayClass>
+    suspend fun getAllSaturdayClasses():List<MondayClass>
+    suspend fun getAllSundayClasses():List<MondayClass>
 
 }
