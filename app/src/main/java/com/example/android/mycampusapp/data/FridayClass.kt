b55daf46cb89a09a8da41a8f0309f3309af866d8
Raphael Ndonga <nkibi53@gmail.com>
2020-06-25 15:20:46 +0300

    This version of My Campus App does not implement hilt and does not intend to do so because of the increased difficulty in testing

--- app/src/main/java/com/example/android/mycampusapp/data/FridayClass.kt
+++ app/src/main/java/com/example/android/mycampusapp/data/FridayClass.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "friday_table")
 data class FridayClass(
     @PrimaryKey(autoGenerate = true)
-    var id: Long = 0,
+    var id: Long = 0, 
     @ColumnInfo(name = "subject")
     var subject:String,
     @ColumnInfo(name = "time")
