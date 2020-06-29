4a4e1d509a8392cedfb41cb31b12d0026543692c
Raphael Ndonga <nkibi53@gmail.com>
2020-06-29 13:06:55 +0300

    Snackbar put in place and Test Driven Development Enforced. A fake repository was created. ClassInputViewModel.kt was tested adequately. The respective snackbars were also put in place correctly. Things are going smoothly

--- app/src/main/java/com/example/android/mycampusapp/classInput/ClassInputViewModel.kt
+++ app/src/main/java/com/example/android/mycampusapp/classInput/ClassInputViewModel.kt
@@ -1,22 +1,23 @@
 package com.example.android.mycampusapp.classInput
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.mycampusapp.Event
+import com.example.android.mycampusapp.R
 import com.example.android.mycampusapp.data.MondayClass
-import com.example.android.mycampusapp.data.timetable.local.TimetableDao
 import com.example.android.mycampusapp.data.timetable.local.TimetableDataSource
-import com.example.android.mycampusapp.data.timetable.local.TimetableLocalDataSource
 import kotlinx.coroutines.*
 
-class ClassInputViewModel(private val timetableRepository: TimetableLocalDataSource) : ViewModel() {
+class ClassInputViewModel(private val timetableRepository: TimetableDataSource) : ViewModel() {
 
     enum class Days { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY }
 
-    val job = Job()
-    val uiScope = CoroutineScope(Dispatchers.Main + job)
+    private val _navigator = MutableLiveData<Event<Unit>>()
+    val navigator:LiveData<Event<Unit>>
+        get() = _navigator
+    private val job = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + job)
 
     val subject = MutableLiveData<String>()
 
@@ -26,9 +27,8 @@ class ClassInputViewModel(private val timetableRepository: TimetableLocalDataSou
     val status: LiveData<Days>
         get() = _status
 
-    //TODO change this to Event<int> once you're done testing stuff
-    private val _snackbarText = MutableLiveData<Event<String>>()
-    val snackbarText: LiveData<Event<String>>
+    private val _snackbarText = MutableLiveData<Event<Int>>()
+    val snackbarText: LiveData<Event<Int>>
         get() = _snackbarText
 
 
@@ -36,15 +36,21 @@ class ClassInputViewModel(private val timetableRepository: TimetableLocalDataSou
         val currentSubject: String? = subject.value
         val currentTime: String? = time.value
         if (currentSubject.isNullOrBlank() || currentTime.isNullOrBlank()) {
-            Log.i("ClassInputViewModel", "please fill in all the blanks")
-            return
+            _snackbarText.value = Event(R.string.empty_message)
         } else {
-
+            //TODO add all the other weekday classes in a when statement
+            addMondayClass(currentSubject,currentTime)
+            navigateToTimetable()
         }
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        timetableRepository.job.cancel()
+    fun navigateToTimetable() {
+        _navigator.value = Event(Unit)
     }
+
+    fun addMondayClass(subject: String, time: String) = uiScope.launch {
+        timetableRepository.addMondayClass(MondayClass(subject = subject,time = time))
+        _snackbarText.value = Event(R.string.monday_saved)
+    }
+    //TODO add all the other weekday classes and their functions
 }
