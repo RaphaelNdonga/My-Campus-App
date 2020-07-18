6774b5de30509f2bce6fa4f7f900a35952c7723b
Raphael Ndonga <nkibi53@gmail.com>
2020-07-18 15:30:11 +0300

    Timber planted

--- app/src/main/java/com/example/android/mycampusapp/MyCampusApplication.kt
+++ app/src/main/java/com/example/android/mycampusapp/MyCampusApplication.kt
@@ -2,6 +2,12 @@ package com.example.android.mycampusapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MyCampusApplication:Application()
+class MyCampusApplication : Application() {
+    override fun onCreate() {
+        Timber.plant(Timber.DebugTree())
+        super.onCreate()
+    }
+}
