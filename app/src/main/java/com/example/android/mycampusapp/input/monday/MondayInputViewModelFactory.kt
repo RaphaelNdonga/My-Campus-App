139075d9a03a03cee75c12c2e95b96d737af12bc
Raphael Ndonga <nkibi53@gmail.com>
2020-07-19 07:47:05 +0300

    Perfect Timing for toast with everything in place. I need this as a separate copy so that I can manipulate the master for testing

--- app/src/main/java/com/example/android/mycampusapp/input/monday/MondayInputViewModelFactory.kt
+++ app/src/main/java/com/example/android/mycampusapp/input/monday/MondayInputViewModelFactory.kt
@@ -1,19 +1,18 @@
-package com.example.android.mycampusapp.classInput
+package com.example.android.mycampusapp.input.monday
 
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.mycampusapp.data.MondayClass
-import com.example.android.mycampusapp.data.timetable.local.TimetableDao
 import com.example.android.mycampusapp.data.timetable.local.TimetableDataSource
-import com.example.android.mycampusapp.data.timetable.local.TimetableLocalDataSource
 
-class ClassInputViewModelFactory(
+class MondayInputViewModelFactory(
     private val timetableRepository: TimetableDataSource,
     private val mondayClass: MondayClass?,
     private val app:Application
 ) : ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return ClassInputViewModel(timetableRepository, mondayClass, app) as T
+        return MondayInputViewModel(timetableRepository, mondayClass, app) as T
     }
 }
