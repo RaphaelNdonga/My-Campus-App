1286b38db1c48e7871f97ec87107d039603da216
Raphael Ndonga <nkibi53@gmail.com>
2020-08-03 14:19:03 +0300

    Drawer layout put in place successfully

--- app/src/main/java/com/example/android/mycampusapp/MainActivity.kt
+++ app/src/main/java/com/example/android/mycampusapp/MainActivity.kt
@@ -1,28 +1,34 @@
 package com.example.android.mycampusapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.mycampusapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout:DrawerLayout
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
         val toolbar = binding.appBar
         this.setSupportActionBar(toolbar)
-        val navController = this.findNavController(R.id.nav_host_fragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        drawerLayout = binding.drawerLayout
+        val navView = binding.navView
+        navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(navView,navController)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController,drawerLayout)
     }
 }
