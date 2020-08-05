4a05f3d60073dbb63cd9d40c45cc18feb7c89b75
Raphael Ndonga <nkibi53@gmail.com>
2020-08-05 14:39:12 +0300

    id changed from long to string. auto-generation enforced by uuid

--- app/src/main/java/com/example/android/mycampusapp/timetable/data/FridayClass.kt
+++ app/src/main/java/com/example/android/mycampusapp/timetable/data/FridayClass.kt
@@ -5,12 +5,13 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
+import java.util.*
 
 @Parcelize
 @Entity(tableName = "friday_table")
 data class FridayClass(
-    @PrimaryKey(autoGenerate = true)
-    var id: Long = 0, 
+    @PrimaryKey
+    var id: String = UUID.randomUUID().toString(),
     @ColumnInfo(name = "subject")
     var subject:String,
     @ColumnInfo(name = "time")
