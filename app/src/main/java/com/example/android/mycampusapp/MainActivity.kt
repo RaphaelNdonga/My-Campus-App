d53767c44e5ab794ac99bd998a1ae7ca8016e994
Raphael Ndonga <nkibi53@gmail.com>
2021-02-09 15:26:50 +0300

    MainActivity.kt will be the position whereby we check automatically whether the user is an admin and also add his data to the firestore collection admin

--- app/src/main/java/com/example/android/mycampusapp/MainActivity.kt
+++ app/src/main/java/com/example/android/mycampusapp/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.example.android.mycampusapp
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.android.mycampusapp.data.AdminEmail
 import com.example.android.mycampusapp.databinding.ActivityMainBinding
 import com.example.android.mycampusapp.timetable.service.TimetableService
+import com.example.android.mycampusapp.util.COURSE_ID
+import com.example.android.mycampusapp.util.IS_ADMIN
+import com.example.android.mycampusapp.util.USER_EMAIL
+import com.example.android.mycampusapp.util.sharedPrefFile
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.ListenerRegistration
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navController: NavController
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var snapshotListener: ListenerRegistration
+    private lateinit var viewModel: AdminAccountsViewModel
+
+    @Inject
+    lateinit var courseCollection: CollectionReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val serviceIntent = Intent(this, TimetableService::class.java)
         stopService(serviceIntent)
@@ -33,6 +51,35 @@ class MainActivity : AppCompatActivity() {
         navController = this.findNavController(R.id.nav_host_fragment)
         NavigationUI.setupWithNavController(navView, navController)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        sharedPreferences = this.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        val courseId = sharedPreferences.getString(COURSE_ID, "")!!
+        val userEmail = sharedPreferences.getString(USER_EMAIL, "")!!
+        val isAdmin = sharedPreferences.getBoolean(IS_ADMIN, false)
+
+        val adminEmail = AdminEmail(userEmail)
+        viewModel = ViewModelProvider(
+            this,
+            AdminAccountsViewModelFactory(
+                courseCollection.document(courseId).collection("admins")
+            )
+        ).get(AdminAccountsViewModel::class.java)
+
+        viewModel.adminList.observe(this, {
+            if (isAdmin)
+                viewModel.checkAndAddEmail(it,adminEmail)
+        })
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        snapshotListener = viewModel.addSnapshotListener()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        snapshotListener.remove()
     }
 
     override fun onSupportNavigateUp(): Boolean {
