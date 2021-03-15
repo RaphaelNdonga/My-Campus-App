1e1822c2f20d6c4d71dd62ae361c6b20715c304e
Raphael Ndonga <nkibi53@gmail.com>
2021-03-15 08:29:28 +0300

    MyFirebaseMessagingService.kt edited to send notification to subscribed users

--- app/src/main/java/com/example/android/mycampusapp/timetable/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/mycampusapp/timetable/service/MyFirebaseMessagingService.kt
@@ -1,17 +1,32 @@
 package com.example.android.mycampusapp.timetable.service
 
+import android.app.NotificationManager
+import androidx.core.content.ContextCompat
+import com.example.android.mycampusapp.util.sendNotification
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import timber.log.Timber
 
-class MyFirebaseMessagingService:FirebaseMessagingService() {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
         Timber.i("A new message has been received from ${remoteMessage.from}")
         Timber.i("The message is ${remoteMessage.data}")
+        Timber.i("The notification is ${remoteMessage.data["message"]}")
+        val notificationMessage = remoteMessage.data["message"]
+        notificationMessage?.let { sendNotification(it) }
     }
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Timber.i("A new token has been received $token")
+    }
+
+    private fun sendNotification(message: String) {
+        val notificationManager = ContextCompat.getSystemService(
+            applicationContext,
+            NotificationManager::class.java
+        ) as NotificationManager
+        notificationManager.sendNotification(message, applicationContext)
     }
 }
