57a4d1780770941025aa21115a417730ecee50e9
Raphael Ndonga <nkibi53@gmail.com>
2021-03-15 09:36:27 +0300

    createNewClass and update functions created

--- app/src/main/java/com/example/android/mycampusapp/timetable/input/TimetableInputViewModel.kt
+++ app/src/main/java/com/example/android/mycampusapp/timetable/input/TimetableInputViewModel.kt
@@ -1,10 +1,7 @@
 package com.example.android.mycampusapp.timetable.input
 
-import android.app.AlarmManager
 import android.app.Application
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -12,14 +9,11 @@ import com.example.android.mycampusapp.R
 import com.example.android.mycampusapp.data.CustomTime
 import com.example.android.mycampusapp.data.Location
 import com.example.android.mycampusapp.data.TimetableClass
-import com.example.android.mycampusapp.timetable.receiver.FridayClassReceiver
 import com.example.android.mycampusapp.util.*
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.functions.FirebaseFunctions
 import timber.log.Timber
-import java.text.ParseException
-import java.text.SimpleDateFormat
 import java.util.*
 
 class TimetableInputViewModel(
@@ -51,10 +45,6 @@ class TimetableInputViewModel(
         get() = _timeSet
 
     private val cal: Calendar = Calendar.getInstance()
-    private val hour = cal.get(Calendar.HOUR_OF_DAY)
-    private val minute = cal.get(Calendar.MINUTE)
-    private val day = cal.get(Calendar.DAY_OF_WEEK)
-    private val friday = Calendar.FRIDAY
 
     private val _snackbarText = MutableLiveData<Event<Int>>()
     val snackbarText: LiveData<Event<Int>>
@@ -74,7 +64,7 @@ class TimetableInputViewModel(
             return
         } else if (timetableClass == null) {
             //Create new class
-            val fridayClass =
+            val timetableClass =
                 TimetableClass(
                     subject = currentSubject,
                     hour = currentTime.hour,
@@ -83,17 +73,10 @@ class TimetableInputViewModel(
                     locationCoordinates = currentLocation.coordinates,
                     room = currentRoom
                 )
-            addFirestoreData(fridayClass)
-            _snackbarText.value = Event(R.string.class_saved)
-            startTimer(fridayClass)
-            navigateToTimetable()
-            val notificationMessage =
-                "$currentSubject class is set to start at ${formatTime(currentTime)} in ${currentLocation.name} Room $currentRoom"
-            sendCloudMessage(notificationMessage, courseId)
-
+            createNewClass(timetableClass)
         } else {
             //update present class
-            val fridayClass =
+            val timetableClass =
                 TimetableClass(
                     id!!,
                     currentSubject,
@@ -104,14 +87,32 @@ class TimetableInputViewModel(
                     alarmRequestCode!!,
                     currentRoom
                 )
-            addFirestoreData(fridayClass)
-            _snackbarText.value = Event(R.string.friday_updated)
-            startTimer(fridayClass)
-            navigateToTimetable()
-            val notificationMessage =
-                "$currentSubject details have changed. It is set to start at ${formatTime(currentTime)} in ${currentLocation.name} Room $currentRoom"
-            sendCloudMessage(notificationMessage, courseId)
+            updatePresentClass(timetableClass)
         }
+    }
+
+    private fun updatePresentClass(timetableClass: TimetableClass) {
+        addFirestoreData(timetableClass)
+        _snackbarText.value = Event(R.string.friday_updated)
+        navigateToTimetable()
+        val notificationMessage =
+            "${timetableClass.subject} details have changed. It is set to start at ${
+                formatTime(
+                    _timeSet.value!!
+                )
+            } in ${timetableClass.locationName} Room ${timetableClass.room}"
+        sendCloudMessage(notificationMessage, courseId)
+    }
+
+    private fun createNewClass(timetableClass: TimetableClass) {
+        addFirestoreData(timetableClass)
+        _snackbarText.value = Event(R.string.class_saved)
+        navigateToTimetable()
+        val notificationMessage =
+            "${timetableClass.subject} class is set to start at ${
+                formatTime(_timeSet.value!!)
+            } in ${timetableClass.locationName} Room ${timetableClass.room}"
+        sendCloudMessage(notificationMessage, courseId)
     }
 
     private fun addFirestoreData(fridayClass: TimetableClass) {
@@ -124,44 +125,6 @@ class TimetableInputViewModel(
 
     private fun navigateToTimetable() {
         _displayNavigator.value = Event(Unit)
-    }
-
-
-    private fun startTimer(fridayClass: TimetableClass) {
-        val time = try {
-            SimpleDateFormat("hh:mm a", Locale.US).parse(textBoxTime.value!!)
-        } catch (parseException: ParseException) {
-            Timber.i("The exception caught is $parseException")
-            SimpleDateFormat("HH:mm", Locale.UK).parse(textBoxTime.value!!)
-        }
-        val calendar = Calendar.getInstance()
-        calendar.time = time!!
-        initializeTimetableCalendar(calendar)
-
-        if (calendar.timeInMillis <= System.currentTimeMillis() && day == friday
-        ) {
-            calendar.set(Calendar.WEEK_OF_MONTH, calendar.get(Calendar.WEEK_OF_MONTH + 1))
-        }
-        val triggerTime = calendar.timeInMillis
-
-        val notifyIntent = Intent(app, FridayClassReceiver::class.java).apply {
-            putExtra("fridaySubject", textBoxSubject.value)
-            putExtra("fridayTime", textBoxTime.value)
-        }
-        val notifyPendingIntent = PendingIntent.getBroadcast(
-            getApplication(),
-            fridayClass.alarmRequestCode,
-            notifyIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT
-        )
-        val alarmManager = app.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager.setRepeating(
-            AlarmManager.RTC_WAKEUP,
-            triggerTime,
-            RUN_DAILY,
-            notifyPendingIntent
-        )
-
     }
 
     fun setLocation(loc: Location) {
