66bd4afd1f8f91d7106546fa709a3f7025cdba21
Raphael Ndonga <nkibi53@gmail.com>
2021-03-18 12:00:42 +0300

    containWhiteSpace utility added in StringUtil.kt

--- app/src/main/java/com/example/android/mycampusapp/util/StringUtil.kt
+++ app/src/main/java/com/example/android/mycampusapp/util/StringUtil.kt
@@ -3,4 +3,5 @@ package com.example.android.mycampusapp.util
 import android.util.Patterns
 
 fun String?.isValidEmail() = !isNullOrEmpty()&& Patterns.EMAIL_ADDRESS.matcher(this).matches()
-fun String.removeWhiteSpace() = replace("\\s".toRegex(),"")
+fun String.removeWhiteSpace() = replace("\\s".toRegex(), "")
+fun String.containsWhiteSpace() = contains("\\s".toRegex())
