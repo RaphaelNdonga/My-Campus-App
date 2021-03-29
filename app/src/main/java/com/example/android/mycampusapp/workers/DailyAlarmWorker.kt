277f0957bf2a973fbad3726d5a51aca1065da793
Raphael Ndonga <nkibi53@gmail.com>
2021-03-29 17:42:19 +0300

    This work manager works!

--- app/src/main/java/com/example/android/mycampusapp/workers/DailyAlarmWorker.kt
+++ app/src/main/java/com/example/android/mycampusapp/workers/DailyAlarmWorker.kt
@@ -5,6 +5,7 @@ import androidx.hilt.work.HiltWorker
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.android.mycampusapp.data.TimetableClass
+import com.example.android.mycampusapp.location.LocationUtils
 import com.example.android.mycampusapp.util.COURSE_ID
 import com.example.android.mycampusapp.util.getTomorrowEnumDay
 import com.example.android.mycampusapp.util.sharedPrefFile
@@ -21,25 +22,39 @@ class DailyAlarmWorker @AssistedInject constructor(
 ) : Worker(context, workerParams) {
     @Inject
     lateinit var coursesCollection: CollectionReference
-    private val classList = mutableListOf<TimetableClass>()
 
     override fun doWork(): Result {
         val sharedPreferences =
             applicationContext.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
         val courseId = sharedPreferences.getString(COURSE_ID, "")!!
 
+        val location = LocationUtils.getJkuatLocations()[0]
+
         val calendar = Calendar.getInstance()
         val today = calendar.get(Calendar.DAY_OF_WEEK)
         val hour = calendar.get(Calendar.HOUR_OF_DAY)
         val minute = calendar.get(Calendar.MINUTE)
-        val timetableQuerySnapshot =
-            coursesCollection.document(courseId).collection(getTomorrowEnumDay().name).get()
 
-        timetableQuerySnapshot.addOnSuccessListener { timetableClasses ->
-            timetableClasses.forEach { timetableClass ->
-                classList.add(timetableClass.toObject(TimetableClass::class.java))
-            }
-        }
+        val workerClass = TimetableClass(
+            subject = "WorkManager",
+            hour = hour,
+            minute = minute,
+            locationName = location.name,
+            locationCoordinates = location.coordinates,
+            room = "101"
+        )
+
+        addFirestoreData(workerClass, courseId)
+
         return Result.success()
+    }
+
+    companion object {
+        const val WORKER_NAME = "DailyAlarmWorker"
+    }
+
+    private fun addFirestoreData(timetableClass: TimetableClass, courseId: String) {
+        coursesCollection.document(courseId).collection(getTomorrowEnumDay().name)
+            .document(timetableClass.id).set(timetableClass)
     }
 }
