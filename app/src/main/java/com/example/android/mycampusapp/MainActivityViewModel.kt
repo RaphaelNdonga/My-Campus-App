96709a4c8a3ffd1a0dc15694227136d366f1eeee
Raphael Ndonga <nkibi53@gmail.com>
2021-03-29 18:02:58 +0300

    Renamed from AdminAccountsViewModel.kt It has also been noted that the WorkManager is best instantiated from Dispatchers.Default

--- app/src/main/java/com/example/android/mycampusapp/MainActivityViewModel.kt
+++ app/src/main/java/com/example/android/mycampusapp/MainActivityViewModel.kt
@@ -1,19 +1,29 @@
 package com.example.android.mycampusapp
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.work.*
 import com.example.android.mycampusapp.data.AdminEmail
+import com.example.android.mycampusapp.workers.DailyAlarmWorker
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.ListenerRegistration
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
-class AdminAccountsViewModel(
+class MainActivityViewModel(
     private val adminCollection: CollectionReference,
-) : ViewModel() {
+    private val app: Application
+) : AndroidViewModel(app) {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     private val _adminList = MutableLiveData<List<AdminEmail>>()
-    val adminList:LiveData<List<AdminEmail>>
+    val adminList: LiveData<List<AdminEmail>>
         get() = _adminList
 
     fun addSnapshotListener(): ListenerRegistration {
@@ -37,4 +47,22 @@ class AdminAccountsViewModel(
         // add the email to the collection
         adminCollection.document(userEmail.email).set(userEmail)
     }
+
+    fun setupRecurringWork() {
+        applicationScope.launch {
+            val constraints =
+                Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+
+            val periodicWorkRequest = PeriodicWorkRequestBuilder<DailyAlarmWorker>(
+                1, TimeUnit.DAYS
+            ).setConstraints(constraints).build()
+
+            WorkManager.getInstance(app).enqueueUniquePeriodicWork(
+                DailyAlarmWorker.WORKER_NAME,
+                ExistingPeriodicWorkPolicy.KEEP,
+                periodicWorkRequest
+            )
+        }
+    }
+
 }
