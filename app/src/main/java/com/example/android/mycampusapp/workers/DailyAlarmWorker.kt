af15bf9c4316d998b938531903ad07e2fbedc099
Raphael Ndonga <nkibi53@gmail.com>
2021-04-05 17:07:00 +0300

    This DailyAlarmWorker.kt works perfectly. It alerts the user of classes that are one day from now

--- app/src/main/java/com/example/android/mycampusapp/workers/DailyAlarmWorker.kt
+++ app/src/main/java/com/example/android/mycampusapp/workers/DailyAlarmWorker.kt
@@ -7,9 +7,11 @@ import android.content.Intent
 import androidx.hilt.work.HiltWorker
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.android.mycampusapp.data.CustomDate
+import com.example.android.mycampusapp.data.CustomTime
 import com.example.android.mycampusapp.data.TimetableClass
 import com.example.android.mycampusapp.timetable.receiver.TimetableAlarmReceiver
-import com.example.android.mycampusapp.util.getTomorrowEnumDay
+import com.example.android.mycampusapp.util.*
 import com.google.firebase.firestore.CollectionReference
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
@@ -26,22 +28,57 @@ class DailyAlarmWorker @AssistedInject constructor(
     lateinit var coursesCollection: CollectionReference
 
     override fun doWork(): Result {
-        val calendar = Calendar.getInstance()
-        calendar.add(Calendar.DAY_OF_WEEK, 1)
+        val sharedPreferences =
+            applicationContext.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        val courseId = sharedPreferences.getString(COURSE_ID, "")!!
 
-        Timber.i("The daily worker is set to alert at${calendar.get(Calendar.DAY_OF_WEEK)}")
+        val tomorrowTimetableClasses =
+            coursesCollection.document(courseId).collection(getTomorrowEnumDay().name).get()
 
-        val intent = Intent(applicationContext, TimetableAlarmReceiver::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(
-            applicationContext,
-            0,
-            intent,
-            PendingIntent.FLAG_UPDATE_CURRENT
-        )
+        tomorrowTimetableClasses.addOnSuccessListener {
+            it.forEach { documentSnapshot ->
+                val tomorrowClass = documentSnapshot.toObject(TimetableClass::class.java)
+                val intent = Intent(applicationContext, TimetableAlarmReceiver::class.java)
+                val message = "${tomorrowClass.subject} starts at ${
+                    formatTime(
+                        CustomTime(
+                            tomorrowClass.hour,
+                            tomorrowClass.minute
+                        )
+                    )
+                } in ${tomorrowClass.locationName} room ${tomorrowClass.room}"
+                intent.putExtra("message", message)
+                val pendingIntent = PendingIntent.getBroadcast(
+                    applicationContext,
+                    tomorrowClass.alarmRequestCode,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+                val tomorrowCalendar = getTomorrowTimetableCalendar(tomorrowClass)
 
-        val alarmManager =
-            applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+                val alarmManager =
+                    applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                alarmManager.setExact(
+                    AlarmManager.RTC_WAKEUP,
+                    tomorrowCalendar.timeInMillis,
+                    pendingIntent
+                )
+                val tomorrowTime = CustomTime(
+                    tomorrowCalendar.get(Calendar.HOUR_OF_DAY),
+                    tomorrowCalendar.get(Calendar.MINUTE)
+                )
+                val tomorrowDate = CustomDate(
+                    tomorrowCalendar.get(Calendar.YEAR),
+                    tomorrowCalendar.get(Calendar.MONTH),
+                    tomorrowCalendar.get(Calendar.DAY_OF_MONTH)
+                )
+                Timber.i(
+                    "Alarm set for date ${formatDate(tomorrowDate)} at time ${
+                        formatTime(tomorrowTime)
+                    }  "
+                )
+            }
+        }
 
         return Result.success()
     }
