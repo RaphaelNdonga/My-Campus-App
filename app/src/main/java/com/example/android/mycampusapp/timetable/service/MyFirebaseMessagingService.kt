49aa5c78c15940fab818e9e4babf68f3d8c9526c
Raphael Ndonga <nkibi53@gmail.com>
2021-04-09 10:40:26 +0300

    The alarm is cancelled effectively when deleted

--- app/src/main/java/com/example/android/mycampusapp/timetable/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/mycampusapp/timetable/service/MyFirebaseMessagingService.kt
@@ -1,7 +1,11 @@
 package com.example.android.mycampusapp.timetable.service
 
+import android.app.AlarmManager
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.core.content.ContextCompat
+import com.example.android.mycampusapp.timetable.receiver.TimetableAlarmReceiver
 import com.example.android.mycampusapp.util.sendNotification
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -13,14 +17,23 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         Timber.i("A new message has been received from ${remoteMessage.from}")
         Timber.i("The message is ${remoteMessage.data}")
         val notificationMessage = remoteMessage.data["message"]
-        val notificationId = remoteMessage.data["setAlarmId"]
+        val setAlarmId = remoteMessage.data["setAlarmId"]
         val cancelAlarmId = remoteMessage.data["cancelAlarmId"]
         notificationMessage?.let { sendNotification(it) }
-        notificationId?.let {
-            Timber.i("The notification id is $it")
+        val intent = Intent(applicationContext, TimetableAlarmReceiver::class.java)
+        setAlarmId?.let { requestCode ->
+            Timber.i("The notification id is ${requestCode.toInt()}")
         }
-        cancelAlarmId?.let {
-            Timber.i("The cancel alarm id is $it")
+        cancelAlarmId?.let { requestCode ->
+            val pendingIntent = PendingIntent.getBroadcast(
+                applicationContext,
+                requestCode.toInt(),
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            val alarmManager = applicationContext.getSystemService(ALARM_SERVICE) as AlarmManager
+            alarmManager.cancel(pendingIntent)
+            Timber.i("The cancel alarm id is ${requestCode.toInt()}")
         }
     }
 
