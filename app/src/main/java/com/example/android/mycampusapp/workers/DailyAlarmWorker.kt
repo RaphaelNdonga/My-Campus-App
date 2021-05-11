801f3638e1048ab8c330b4445c4c8daa9d65b751
Raphael Ndonga <nkibi53@gmail.com>
2021-05-11 19:50:26 +0300

    the DailyAlarmWorker.kt sets today's alarm and tomorrow's alarm also

--- app/src/main/java/com/example/android/mycampusapp/workers/DailyAlarmWorker.kt
+++ app/src/main/java/com/example/android/mycampusapp/workers/DailyAlarmWorker.kt
@@ -8,7 +8,6 @@ import android.text.format.DateFormat
 import androidx.hilt.work.HiltWorker
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.example.android.mycampusapp.data.CustomDate
 import com.example.android.mycampusapp.data.CustomTime
 import com.example.android.mycampusapp.data.TimetableClass
 import com.example.android.mycampusapp.timetable.receiver.TimetableAlarmReceiver
@@ -16,8 +15,6 @@ import com.example.android.mycampusapp.util.*
 import com.google.firebase.firestore.CollectionReference
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
-import timber.log.Timber
-import java.util.*
 import javax.inject.Inject
 
 @HiltWorker
@@ -32,10 +29,72 @@ class DailyAlarmWorker @AssistedInject constructor(
         val sharedPreferences =
             applicationContext.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
         val courseId = sharedPreferences.getString(COURSE_ID, "")!!
-        val dayCollection = getTomorrowEnumDay().name
-        sharedPreferences.edit().putString(ALARM_SET_COLLECTION, dayCollection).apply()
+
+        /**
+         * Why do we set two alarms?
+         * Let's suppose the user installs the app at 9:00 pm.
+         * The daily alarm will get today's classes, but since there are none, no alarms will be
+         * set. Tomorrow, the worker will work at exactly 9:00 pm again, or thereabouts.
+         * Therefore there will be an infinite loop of alarms not set if the alarm sets today's
+         * classes.
+         * That's why we set tomorrow's alarms also.
+         * There is no overhead cost for overlaps in the alarm manager
+         */
+
+        val tomorrowCollection = getTomorrowEnumDay().name
+        val todayCollection = getTodayEnumDay().name
+
+        /**
+         * Why do we need ALARM_SET_COLLECTION?
+         * Let's say we use getTomorrowEnumDay.name to set the alarm and also to clear it through
+         * the clearance worker.
+         * If the alarm is set today and the clearance is done tomorrow before the next worker runs,
+         * what happens?
+         * The clearance will use tomorrow's collection instead of today's.
+         * Therefore, it is necessary to keep track of which collection has been set by the
+         * worker, mainly for clearance purposes. We don't want any leftover collections lying
+         * around.
+         */
+        sharedPreferences.edit().putStringSet(
+            ALARM_SET_COLLECTION, setOf(tomorrowCollection, todayCollection)
+        ).apply()
+
+        val todayClasses = coursesCollection.document(courseId).collection(todayCollection).get()
+
+        todayClasses.addOnSuccessListener {
+            it.forEach { documentSnapshot ->
+                val todayClass = documentSnapshot.toObject(TimetableClass::class.java)
+                if (isLater(todayClass)) {
+                    val intent = Intent(applicationContext, TimetableAlarmReceiver::class.java)
+                    val message = "${todayClass.subject} starts at ${
+                        format24HourTime(
+                            CustomTime(
+                                todayClass.hour,
+                                todayClass.minute
+                            )
+                        )
+                    } in ${todayClass.locationName} room ${todayClass.room}"
+                    intent.putExtra("message", message)
+                    intent.putExtra("dayOfWeek", getTomorrowEnumDay().name)
+                    val pendingIntent = PendingIntent.getBroadcast(
+                        applicationContext,
+                        todayClass.alarmRequestCode,
+                        intent,
+                        PendingIntent.FLAG_CANCEL_CURRENT
+                    )
+                    val alarmManager =
+                        applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                    alarmManager.setExact(
+                        AlarmManager.RTC_WAKEUP,
+                        getTimetableCalendar(todayClass, getTodayEnumDay()).timeInMillis,
+                        pendingIntent
+                    )
+                }
+            }
+        }
+
         val tomorrowTimetableClasses =
-            coursesCollection.document(courseId).collection(dayCollection).get()
+            coursesCollection.document(courseId).collection(tomorrowCollection).get()
 
         tomorrowTimetableClasses.addOnSuccessListener {
             it.forEach { documentSnapshot ->
@@ -66,20 +125,6 @@ class DailyAlarmWorker @AssistedInject constructor(
                     tomorrowCalendar.timeInMillis,
                     pendingIntent
                 )
-                val tomorrowTime = CustomTime(
-                    tomorrowCalendar.get(Calendar.HOUR_OF_DAY),
-                    tomorrowCalendar.get(Calendar.MINUTE)
-                )
-                val tomorrowDate = CustomDate(
-                    tomorrowCalendar.get(Calendar.YEAR),
-                    tomorrowCalendar.get(Calendar.MONTH),
-                    tomorrowCalendar.get(Calendar.DAY_OF_MONTH)
-                )
-                Timber.i(
-                    "Alarm set for date ${formatDate(tomorrowDate)} at time ${
-                        formatTime(tomorrowTime)
-                    }  "
-                )
             }
         }
 
@@ -96,10 +141,5 @@ class DailyAlarmWorker @AssistedInject constructor(
 
     companion object {
         const val WORKER_NAME = "DailyAlarmWorker"
-    }
-
-    private fun addFirestoreData(timetableClass: TimetableClass, courseId: String) {
-        coursesCollection.document(courseId).collection(getTomorrowEnumDay().name)
-            .document(timetableClass.id).set(timetableClass)
     }
 }
