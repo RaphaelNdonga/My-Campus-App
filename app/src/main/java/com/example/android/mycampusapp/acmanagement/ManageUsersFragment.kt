2d0220852632d0f84967fc3a548c837f6ce698f6
Raphael Ndonga <nkibi53@gmail.com>
2021-05-21 11:05:32 +0300

    viewpager and tablayouts added

--- app/src/main/java/com/example/android/mycampusapp/acmanagement/ManageUsersFragment.kt
+++ app/src/main/java/com/example/android/mycampusapp/acmanagement/ManageUsersFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.android.mycampusapp.databinding.FragmentManageUsersBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class ManageUsersFragment : Fragment() {
     private lateinit var binding: FragmentManageUsersBinding
@@ -15,6 +16,17 @@ class ManageUsersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentManageUsersBinding.inflate(layoutInflater, container, false)
+        val tabLayout = binding.tabLayout
+        val viewPager = binding.viewPager
+
+        viewPager.adapter = ManageUsersAdapter(this)
+
+        val users = listOf("regular", "admin")
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = users[position]
+        }.attach()
+
         return binding.root
     }
 }
