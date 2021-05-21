b6021c8398a5bd88d06a21d3b140702c87f77293
Raphael Ndonga <nkibi53@gmail.com>
2021-05-21 12:12:49 +0300

    AdminsFragment.kt uses a different layout file

--- app/src/main/java/com/example/android/mycampusapp/acmanagement/AdminsFragment.kt
+++ app/src/main/java/com/example/android/mycampusapp/acmanagement/AdminsFragment.kt
@@ -6,27 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.android.mycampusapp.R
+import com.example.android.mycampusapp.databinding.UsersFragmentBinding
 
 class AdminsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = AdminsFragment()
-    }
-
     private lateinit var viewModel: AdminsViewModel
+    private lateinit var binding: UsersFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.admins_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        binding = UsersFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(AdminsViewModel::class.java)
-        // TODO: Use the ViewModel
+        return binding.root
     }
-
 }
