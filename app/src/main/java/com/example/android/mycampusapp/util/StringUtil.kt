4f7da06ddf7825098abb3c5e69605a7cab463e29
Raphael Ndonga <nkibi53@gmail.com>
2021-05-25 19:38:18 +0300

    Using patterns to determine whether a link is a valid url

--- app/src/main/java/com/example/android/mycampusapp/util/StringUtil.kt
+++ app/src/main/java/com/example/android/mycampusapp/util/StringUtil.kt
@@ -4,3 +4,4 @@ import android.util.Patterns
 
 fun String.isValidEmail() = Patterns.EMAIL_ADDRESS.matcher(this).matches()
 fun String.isValidMessagingTopic() = contains("[^a-zA-Z0-9-_.~%]".toRegex())
+fun String.isValidUrl() = Patterns.WEB_URL.matcher(this).matches()
