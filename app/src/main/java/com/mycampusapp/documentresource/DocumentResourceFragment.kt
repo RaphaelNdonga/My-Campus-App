276462c0e211fa177d526249d4a2cca26818b5fd
RaphaelNdonga <nkibi53@gmail.com>
2021-07-17 20:45:12 +0300

    layout for document resources created. All that is remaining is to learn how to read that saved data. Since here, glide will not be used

---
+++ app/src/main/java/com/mycampusapp/documentresource/DocumentResourceFragment.kt
@@ -0,0 +1,28 @@
+package com.mycampusapp.documentresource
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mycampusapp.R
+
+class DocumentResourceFragment : Fragment() {
+
+    private lateinit var viewModel: DocumentResourceViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.document_resource_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DocumentResourceViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/mycampusapp/documentresource/DocumentResourceViewModel.kt
@@ -0,0 +1,7 @@
+package com.mycampusapp.documentresource
+
+import androidx.lifecycle.ViewModel
+
+class DocumentResourceViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
