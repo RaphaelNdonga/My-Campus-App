676b7249e59fa2125da55fab902917ff3674faf1
RaphaelNdonga <nkibi53@gmail.com>
2021-07-26 13:56:16 +0300

    DocumentData replaces ImageData

--- app/src/main/java/com/mycampusapp/imageresource/ImageResourceAdapter.kt
+++ app/src/main/java/com/mycampusapp/imageresource/ImageResourceAdapter.kt
@@ -1,29 +1,44 @@
 package com.mycampusapp.imageresource
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.core.net.toUri
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.mycampusapp.R
-import com.mycampusapp.data.ImageData
+import com.mycampusapp.data.DocumentData
 import com.mycampusapp.databinding.ListItemImageBinding
+import com.mycampusapp.documentresource.DocumentsAdapter
+import java.io.File
 
 class ImageResourceAdapter :
-    ListAdapter<ImageData, ImageResourceAdapter.ImageViewHolder>(ImageDiffUtilCallback) {
+    ListAdapter<DocumentData, ImageResourceAdapter.ImageViewHolder>(DocumentsAdapter.DocumentsDiffUtilCallback) {
     class ImageViewHolder(private val binding: ListItemImageBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(imageData: ImageData) {
-            val imageUri = imageData.imageUrl.toUri().buildUpon().scheme("https").build()
-            Glide.with(itemView).load(imageUri).apply(
-                RequestOptions().placeholder(R.drawable.loading_animation)
-                    .error(R.drawable.ic_broken_image)
-            ).into(binding.imageResource)
-        }
+        fun bind(imageData: DocumentData) {
+            val root = binding.root.context.getExternalFilesDir(null).toString()
+            val imageFile = File(root,imageData.fileName)
+            if(imageFile.exists().not()) {
+                binding.greenCheck.visibility = View.GONE
+                val imageUri = imageData.url.toUri().buildUpon().scheme("https").build()
+                Glide.with(itemView).load(imageUri).apply(
+                    RequestOptions().placeholder(R.drawable.loading_animation)
+                        .error(R.drawable.ic_broken_image)
+                ).into(binding.imageResource)
 
+            }else{
+                binding.greenCheck.visibility = View.VISIBLE
+                val imageUri = imageFile.toUri()
+                Glide.with(itemView).load(imageUri).apply(
+                    RequestOptions().placeholder(R.drawable.loading_animation)
+                        .error(R.drawable.ic_broken_image)
+                ).into(binding.imageResource)
+            }
+            binding.imageName.text = imageData.fileName
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
@@ -35,15 +50,4 @@ class ImageResourceAdapter :
         val currentItem = getItem(position)
         holder.bind(currentItem)
     }
-}
-
-object ImageDiffUtilCallback : DiffUtil.ItemCallback<ImageData>() {
-    override fun areItemsTheSame(oldItem: ImageData, newItem: ImageData): Boolean {
-        return oldItem == newItem
-    }
-
-    override fun areContentsTheSame(oldItem: ImageData, newItem: ImageData): Boolean {
-        return false
-    }
-
 }
