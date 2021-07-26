97a477769ca98cdfd81fbe90b0f8a901bcc99c15
RaphaelNdonga <nkibi53@gmail.com>
2021-07-26 13:54:45 +0300

    getFileName functionality added to ImageResourceViewModel.kt

--- app/src/main/java/com/mycampusapp/imageresource/ImageResourceViewModel.kt
+++ app/src/main/java/com/mycampusapp/imageresource/ImageResourceViewModel.kt
@@ -1,16 +1,17 @@
 package com.mycampusapp.imageresource
 
+import android.content.ContentResolver
 import android.content.SharedPreferences
+import android.net.Uri
+import android.provider.OpenableColumns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.ListenerRegistration
-import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.mycampusapp.data.ImageData
+import com.mycampusapp.data.DocumentData
 import com.mycampusapp.util.COURSE_ID
 import com.mycampusapp.util.IMAGES
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -20,30 +21,51 @@ import javax.inject.Inject
 @HiltViewModel
 class ImageResourceViewModel @Inject constructor(
     private val firebaseStorage: FirebaseStorage,
-    private val sharedPreferences: SharedPreferences,
-    private val courseCollection:CollectionReference
+    sharedPreferences: SharedPreferences,
+    courseCollection:CollectionReference,
+    private val contentResolver: ContentResolver
 ) : ViewModel() {
     private val courseId = sharedPreferences.getString(COURSE_ID, "")!!
     private val imagesCollection = courseCollection.document(courseId).collection(IMAGES)
-    private val _images = MutableLiveData<List<ImageData>>()
-    val images:LiveData<List<ImageData>> = _images
+    private val _images = MutableLiveData<List<DocumentData>>()
+    val images:LiveData<List<DocumentData>> = _images
 
     fun getImagesRef(): StorageReference {
         return firebaseStorage.reference.child(courseId).child(IMAGES)
     }
 
-    fun addFirestoreData(uriString: String) {
-        val imageUrl = ImageData(imageUrl = uriString)
+    fun addFirestoreData(documentData: DocumentData) {
+        val imageUrl = DocumentData(url = documentData.url,fileName = documentData.fileName)
         imagesCollection.document(imageUrl.id).set(imageUrl)
     }
 
     fun addSnapshotListener():ListenerRegistration{
         return imagesCollection.addSnapshotListener{ querySnapshot, firestoreException ->
-            _images.value = querySnapshot?.toObjects(ImageData::class.java)
+            _images.value = querySnapshot?.toObjects(DocumentData::class.java)
             if(firestoreException != null){
                 Timber.i("The firebase exception is $firestoreException")
             }
         }
     }
-
+    fun getFileName(uri: Uri): String {
+        var result: String? = null
+        if (uri.scheme == "content") {
+            val cursor = contentResolver.query(uri, null, null, null, null)
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                }
+            } finally {
+                cursor?.close()
+            }
+        }
+        if (result == null) {
+            result = uri.path
+            val cut = result!!.lastIndexOf('/')
+            if (cut != -1) {
+                result = result.substring(cut + 1)
+            }
+        }
+        return result
+    }
 }
